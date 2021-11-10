d9655913f7ff82dc157a67f9d991ea7ae9e2a930
wilhamsJW <wilhamsjunior@gmail.com>
2021-11-10 10:58:20 -0300

    style of cards and main screen and componentization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,44 @@
-import './App.css';
+import React from 'react';
+
+import './App.css'
+import './global.css'
+import './main.css'
+
+import Notes from './Components/Notes'
 
 function App() {
   return (
-    
-    <h1>E aewww</h1>
+
+    <div id="app">
+
+      <aside>
+        <strong>Caderno de Notas</strong>
+        <form>
+
+          <div className="input-block">
+            <label htmlFor="title" placeholder="Reunião da Empresa">Título da Anotação</label>
+            <input />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="title">Anotações</label>
+            <textarea placeholder="Cumprir com reunião ás..."></textarea>
+            <input />
+          </div>
+
+          <button type="submit">Salvar</button>
+
+        </form>
+
+      </aside>
+
+      <main>
+        <ul>
+          <Notes />
+        </ul>
+      </main>
+    </div>
+
   );
 }
 
